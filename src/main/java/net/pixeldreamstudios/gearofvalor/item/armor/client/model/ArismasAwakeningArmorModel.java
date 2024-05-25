bdc74fb967c2a69211c1e15da8cc22ce093e1efd
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2024-05-25 14:01:03 +0200

    correct parameter names

--- src/main/java/net/pixeldreamstudios/gearofvalor/item/armor/client/model/ArismasAwakeningArmorModel.java
+++ src/main/java/net/pixeldreamstudios/gearofvalor/item/armor/client/model/ArismasAwakeningArmorModel.java
@@ -4,7 +4,6 @@ import mod.azure.azurelib.model.GeoModel;
 import net.minecraft.resources.ResourceLocation;
 import net.pixeldreamstudios.gearofvalor.GearOfValor;
 import net.pixeldreamstudios.gearofvalor.item.armor.sets.ArismasAwakeningArmorItem;
-import net.pixeldreamstudios.gearofvalor.item.armor.sets.PaladinArmorItem;
 
 public class ArismasAwakeningArmorModel extends GeoModel<ArismasAwakeningArmorItem> {
     private static final ResourceLocation model = new ResourceLocation(GearOfValor.MOD_ID, "geo/armor/arismas_awakening.geo.json");
@@ -12,17 +11,17 @@ public class ArismasAwakeningArmorModel extends GeoModel<ArismasAwakeningArmorIt
     private static final ResourceLocation animation = new ResourceLocation(GearOfValor.MOD_ID, "animations/armor/arismas_awakening.animation.json");
 
     @Override
-    public ResourceLocation getModelResource(ArismasAwakeningArmorItem paladinArmorItem) {
+    public ResourceLocation getModelResource(ArismasAwakeningArmorItem arismasAwakeningArmorItem) {
         return model;
     }
 
     @Override
-    public ResourceLocation getTextureResource(ArismasAwakeningArmorItem paladinArmorItem) {
+    public ResourceLocation getTextureResource(ArismasAwakeningArmorItem arismasAwakeningArmorItem) {
         return texture;
     }
 
     @Override
-    public ResourceLocation getAnimationResource(ArismasAwakeningArmorItem paladinArmorItem) {
+    public ResourceLocation getAnimationResource(ArismasAwakeningArmorItem arismasAwakeningArmorItem) {
         return animation;
     }
 }
