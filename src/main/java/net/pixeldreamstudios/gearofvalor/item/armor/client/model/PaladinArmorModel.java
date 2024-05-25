074f3d420075b895fe793c7394344be356c1f941
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2024-05-25 19:57:03 +0200

    add cape physics

--- src/main/java/net/pixeldreamstudios/gearofvalor/item/armor/client/model/PaladinArmorModel.java
+++ src/main/java/net/pixeldreamstudios/gearofvalor/item/armor/client/model/PaladinArmorModel.java
@@ -1,12 +1,11 @@
 package net.pixeldreamstudios.gearofvalor.item.armor.client.model;
 
-import mod.azure.azurelib.core.animation.AnimationState;
-import mod.azure.azurelib.model.GeoModel;
 import net.minecraft.resources.ResourceLocation;
 import net.pixeldreamstudios.gearofvalor.GearOfValor;
+import net.pixeldreamstudios.gearofvalor.item.armor.client.model.abstraction.CapeArmorModel;
 import net.pixeldreamstudios.gearofvalor.item.armor.sets.PaladinArmorItem;
 
-public class PaladinArmorModel extends GeoModel<PaladinArmorItem> {
+public class PaladinArmorModel extends CapeArmorModel<PaladinArmorItem> {
     private static final ResourceLocation model = new ResourceLocation(GearOfValor.MOD_ID, "geo/armor/paladin.geo.json");
     private static final ResourceLocation texture = new ResourceLocation(GearOfValor.MOD_ID, "textures/armor/paladin.png");
     private static final ResourceLocation animation = new ResourceLocation(GearOfValor.MOD_ID, "animations/armor/paladin.animation.json");
@@ -24,11 +23,5 @@ public class PaladinArmorModel extends GeoModel<PaladinArmorItem> {
     @Override
     public ResourceLocation getAnimationResource(PaladinArmorItem paladinArmorItem) {
         return animation;
-    }
-
-    @Override
-    public void setCustomAnimations(PaladinArmorItem animatable, long instanceId, AnimationState<PaladinArmorItem> animationState) {
-        super.setCustomAnimations(animatable, instanceId, animationState);
-        var cape = getAnimationProcessor().getBone("cape");
     }
 }
--- src/main/java/net/pixeldreamstudios/gearofvalor/item/armor/client/model/ReapersEmbraceArmorModel.java
+++ src/main/java/net/pixeldreamstudios/gearofvalor/item/armor/client/model/ReapersEmbraceArmorModel.java
@@ -1,11 +1,11 @@
 package net.pixeldreamstudios.gearofvalor.item.armor.client.model;
 
-import mod.azure.azurelib.model.GeoModel;
 import net.minecraft.resources.ResourceLocation;
 import net.pixeldreamstudios.gearofvalor.GearOfValor;
+import net.pixeldreamstudios.gearofvalor.item.armor.client.model.abstraction.CapeArmorModel;
 import net.pixeldreamstudios.gearofvalor.item.armor.sets.ReapersEmbraceArmorItem;
 
-public class ReapersEmbraceArmorModel extends GeoModel<ReapersEmbraceArmorItem> {
+public class ReapersEmbraceArmorModel extends CapeArmorModel<ReapersEmbraceArmorItem> {
     private static final ResourceLocation model = new ResourceLocation(GearOfValor.MOD_ID, "geo/armor/reapers_embrace.geo.json");
     private static final ResourceLocation texture = new ResourceLocation(GearOfValor.MOD_ID, "textures/armor/reapers_embrace.png");
     private static final ResourceLocation animation = new ResourceLocation(GearOfValor.MOD_ID, "animations/armor/reapers_embrace.animation.json");
