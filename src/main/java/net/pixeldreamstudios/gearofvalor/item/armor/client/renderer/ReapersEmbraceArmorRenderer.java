bb340251b6efd51ec068c9daa64c844186e2fa19
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2024-05-25 20:09:31 +0200

    add glowmask to reapers embrace

--- src/main/java/net/pixeldreamstudios/gearofvalor/item/armor/client/renderer/ReapersEmbraceArmorRenderer.java
+++ src/main/java/net/pixeldreamstudios/gearofvalor/item/armor/client/renderer/ReapersEmbraceArmorRenderer.java
@@ -1,11 +1,13 @@
 package net.pixeldreamstudios.gearofvalor.item.armor.client.renderer;
 
 import mod.azure.azurelib.renderer.GeoArmorRenderer;
+import mod.azure.azurelib.renderer.layer.AutoGlowingGeoLayer;
 import net.pixeldreamstudios.gearofvalor.item.armor.client.model.ReapersEmbraceArmorModel;
 import net.pixeldreamstudios.gearofvalor.item.armor.sets.ReapersEmbraceArmorItem;
 
 public class ReapersEmbraceArmorRenderer extends GeoArmorRenderer<ReapersEmbraceArmorItem> {
     public ReapersEmbraceArmorRenderer() {
         super(new ReapersEmbraceArmorModel());
+        addRenderLayer(new AutoGlowingGeoLayer<>(this));
     }
 }
